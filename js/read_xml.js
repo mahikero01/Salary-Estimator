4438fc171d710c13e0a3a48e4d97fb0e94f8bc8b
mahikero01 <federicoparassarmiento@yahoo.com>
2014-03-15 19:32:38 +0800

    Up Files
    read_xml.js

--- js/read_xml.js
+++ js/read_xml.js
@@ -10,4 +10,23 @@ var
 		XMLHttpRequestObject = new ActiveXObject("Microsoft.XMLHTTP");
 	}
 	
-	function getData (  ) {}
+	function getData ( dataSource, divID ) {
+		if ( XMLHttpRequestObject ) {
+			XMLHttpRequestObject.open( "GET", dataSource );
+			var obj = document.getElementById( divID );
+			
+			XMLHttpRequestObject.onreadystatechange = function() {
+				if ( XMLHttpRequestObject.readyState == 4 &&
+						XMLHttpRequestObject.status == 200 ) {
+					var xmlDocument = XMLHttpRequestObject.responseXML;
+					colors = xmlDocument.getElementsByTagName("color");
+					obj.innerHTML = "Here:<ul>";
+					for () {
+					
+					}
+				}
+			};
+			
+			
+		}
+	}
